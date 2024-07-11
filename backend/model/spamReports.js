625a3f8a0f0f4a1e122e14e52b1a44819221265c
unknown <arshamaria2304@gmail.com>
2024-07-11 20:44:21 +0530

    Added backend and frontend folders

---
+++ backend/model/spamReports.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const types = ['SMS', 'Call'];
+
+const spamReportsSchema = new Schema({
+    type: { type: String, enum: types, required: true },
+    phone_number: { type: String, required: true },
+    message: { type: String, default: null },
+    call_info: { type: String, default: null },
+    call_duration: { type: Number, default: null },
+    device_id: { type: String, required: true }, // Add this field
+    report_time: { type: Date, default: Date.now }
+});
+
+<<<<<<< HEAD
+module.exports = mongoose.model('SpamReport', spamReportsSchema);
+=======
+module.exports = mongoose.model('SpamReport', spamReportsSchema);
+>>>>>>> origin/master
